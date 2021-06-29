c55679c3e1aac33964f3ea29c5c3bd1e11268d34
kefalotse <kkehulakae@bitri.co.bw>
2021-06-29 15:10:14 +0200

    project initializing

---
+++ retailStoreDiscounts/src/test/java/com/retailstore/retailStoreDiscounts/RetailStoreDiscountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retailstore.retailStoreDiscounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailStoreDiscountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
